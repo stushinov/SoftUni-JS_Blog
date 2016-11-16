01a16303cfe07e67e8217d903cc432c3d8c7e58c
tanukaaa <tanuka@mail.bg>
2016-11-16 17:45:46 +0200

    We told MnongoDB from which collection to get data from and created 'User - Role' relation

--- models/User.js
+++ models/User.js
@@ -8,7 +8,8 @@ let userSchema = mongoose.Schema(
         email: {type: String, required: true, unique: true},
         passwordHash: {type: String, required: true},
         fullName: {type: String, required: true},
-        articles: {type: [mongoose.Schema.Types.ObjectId], default: []},
+        articles: [{type: mongoose.Schema.Types.ObjectId, ref: 'Article'}],
+        roles: [{type: mongoose.Schema.Types.ObjectId, ref: 'Role'}],
         salt: {type: String, required: true}
     }
 );
